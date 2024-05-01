5bd59a950153fe416c9e9e309a29c618a5e3169c
justin <justin.trisno@gmail.com>
2024-05-02 02:30:04 +0800

    Test database initialization

--- Data-Pipeline/database_init.py
+++ Data-Pipeline/database_init.py
@@ -21,29 +21,34 @@ def fetch_data(endpoint, limit):
 
 def insert_data_to_db(connection, data, table):
     cursor = connection.cursor()
+    insert_query = f"""
+    INSERT INTO {table} (
+        mal_id, title, title_english, title_synonyms, type, status, synopsis, genres, demographics, themes
+    ) VALUES (
+        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+    ) ON CONFLICT (mal_id) DO NOTHING;
+    """
     for item in data:
-        insert_query = f"INSERT INTO {table} (mal_id, title, title_english, title_synonyms, type, status, synopsis, rating, year, genres, demographics, themes, studios) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (mal_id) DO NOTHING;"
-        cursor.execute(
-            insert_query,
-            (
-                item["mal_id"],
-                item["title"],
-                item["title_english"],
-                ", ".join(item["title_synonyms"]),
-                item["type"],
-                item["status"],
-                item["synopsis"],
-                item["rating"],
-                item["year"],
-                ", ".join([genre["name"] for genre in item["genres"]]),
-                ", ".join([demo["name"] for demo in item["demographics"]]),
-                ", ".join([theme["name"] for theme in item["themes"]]),
-                ", ".join([studio["name"] for studio in item["studios"]]),
-            ),
-        )
+        try:
+            cursor.execute(
+                insert_query,
+                (
+                    item["mal_id"],
+                    item["title"],
+                    item["title_english"],
+                    ", ".join(item["title_synonyms"]),
+                    item["type"],
+                    item["status"],
+                    item["synopsis"].replace('\n', ' ').replace('\r', '').replace('"', "'"),
+                    ", ".join([genre["name"] for genre in item["genres"]]),
+                    ", ".join([demo["name"] for demo in item["demographics"]]),
+                    ", ".join([theme["name"] for theme in item["themes"]]),
+                )
+            )
+        except Exception as e:
+            print(f"Error inserting item: {item['title']}. Error: {e}")
     connection.commit()
     cursor.close()
-
 
 def main():
     connection = psycopg2.connect(
@@ -55,9 +60,9 @@ def main():
     )
     try:
         # Fetch and load dataset
-        anime_data = fetch_data("top/anime", 1500)
+        anime_data = fetch_data("top/anime", 50)
         insert_data_to_db(connection, anime_data, "anime_dataset")
-        manga_data = fetch_data("top/manga", 250)
+        manga_data = fetch_data("top/manga", 50)
         insert_data_to_db(connection, manga_data, "manga_dataset")
     finally:
         connection.close()
